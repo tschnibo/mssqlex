ea97e3d1c442cd617c40176be8481aae0f47c338
Julian <tschnibo@gmail.com>
2021-03-09 17:21:24 +0100

    config and protocoll change for more customizability

--- config/config.exs
+++ config/config.exs
@@ -1,1 +1,14 @@
-use Mix.Config
+import Config
+
+
+
+# Config custom drivers in your projects config! Pattern:
+#
+
+for config <- "config/config.exs" |> Path.expand(__DIR__) |> Path.wildcard() do
+  import_config config
+end
+
+
+config :mssqlex,
+  drivername_pingstatement: [%{"name" => ExampleConn, "ping_statement" => "SELECT 1"}]
